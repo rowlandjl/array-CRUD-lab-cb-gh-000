b410d74ce268c0886606b1fb03c12c045a119662
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-19 00:53:45 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array.shift 
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
@@ -27,7 +27,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
